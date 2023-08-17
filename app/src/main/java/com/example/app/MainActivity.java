4af48c9ca26faa2bb083a6321da2898a38ce88df
Imesh Isuranga <imeshisuranga00@gmail.com>
2023-08-17 14:48:45 +0530

    Added Progress Bar

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.app;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     @Override
     @SuppressLint("SetJavaScriptEnabled")
@@ -16,12 +21,31 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = findViewById(R.id.activity_main_webview);
+        mProgressBar = findViewById(R.id.progressBar);
+        mProgressBar.setMax(100);
+        mWebView.loadUrl("https://zahist.in.ua");
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new MyWebViewClient());
 
         // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                findViewById(R.id.progressBar).setVisibility(mProgressBar.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                findViewById(R.id.progressBar).setVisibility(view.GONE);
+            }
+        });
+
+
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
